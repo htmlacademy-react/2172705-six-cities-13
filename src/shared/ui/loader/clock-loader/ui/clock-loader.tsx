6687a225423b0ac09c150e138082efcdb8e6b95a
CendresChaudes <romqaaa1337@gmail.com>
2023-08-14 14:04:12 +0300

    refactor: унифицирует компонент clock-loader

--- src/shared/ui/loader/clock-loader/ui/clock-loader.tsx
+++ src/shared/ui/loader/clock-loader/ui/clock-loader.tsx
@@ -2,14 +2,15 @@ import clsx from 'clsx';
 import styles from './styles.module.css';
 
 type LoaderProps = {
+  text?: string;
   fullPage?: boolean;
 }
 
-export function ClockLoader({ fullPage = false }: LoaderProps) {
+export function ClockLoader({ text = 'Loading data...', fullPage = false }: LoaderProps) {
   return (
     <div className={clsx(styles.wrapper, { [styles['wrapper--page']]: fullPage })}>
       <div className={styles['clock-loader']}></div>
-      <p className={styles.description}>Loading...</p>
+      <p className={styles.description}>{text}</p>
     </div>
   );
 }
